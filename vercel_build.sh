0d2599c86b1adbff99cf65e433d99450209cc115
Rafael Casado <rafael.casado@udc.es>
2026-07-28 19:22:04 +0200

    build: añadir script de compilación para Vercel

---
+++ vercel_build.sh
@@ -0,0 +1,15 @@
+# 1. Crear el archivo vercel_build.sh en la raíz de tu proyecto
+@'
+#!/bin/bash
+mkdir -p lib
+cat <<EOF > lib/env.dart
+class Env {
+  static const String supabaseUrl = '$SUPABASE_URL';
+  static const String supabaseAnonKey = '$SUPABASE_ANON_KEY';
+  static const String igdbClientId = '$IGDB_CLIENT_ID';
+  static const String igdbClientSecret = '$IGDB_CLIENT_SECRET';
+}
+EOF
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+./flutter/bin/flutter build web --release
+'@ | Out-File -Encoding utf8 vercel_build.sh
