6bcdbfe24929cf88bbf5c14acd875b0887bc2bfe
Rafael Casado <rafael.casado@udc.es>
2026-07-28 19:27:23 +0200

    fix: corregir formato bash de vercel_build.sh

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,16 +1,18 @@
-# 1. Crear el archivo vercel_build.sh en la raíz de Corpus
-@'
 #!/bin/bash
+
+# Creamos la carpeta lib por si no existe
 mkdir -p lib
+
+# Generamos el archivo env.dart con las variables que inyectará Vercel
 cat <<EOF > lib/env.dart
 class Env {
-  static const String supabaseUrl = '$SUPABASE_URL';
-  static const String supabaseAnonKey = '$SUPABASE_ANON_KEY';
-  static const String igdbClientId = '$IGDB_CLIENT_ID';
-  static const String igdbClientSecret = '$IGDB_CLIENT_SECRET';
+  static const String supabaseUrl = '${SUPABASE_URL}';
+  static const String supabaseAnonKey = '${SUPABASE_ANON_KEY}';
+  static const String igdbClientId = '${IGDB_CLIENT_ID}';
+  static const String igdbClientSecret = '${IGDB_CLIENT_SECRET}';
 }
 EOF
+
+# Descargamos Flutter y compilamos la versión Web
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 ./flutter/bin/flutter build web --release
-'@ | Out-File -Encoding utf8 vercel_build.sh
-
