3359c9ccf75cca5fe6be711d4fdd43b7956f4a49
Rafael Casado <rafael.casado@udc.es>
2026-08-04 16:56:34 +0200

    arreglado script de compilacion en vercel

--- vercel_build.sh
+++ vercel_build.sh
@@ -3,16 +3,23 @@
 # Creamos la carpeta lib por si no existe
 mkdir -p lib
 
-# Generamos el archivo env.dart con las variables que inyectará Vercel
-cat <<EOF > lib/env.dart
-class Env {
-  static const String supabaseUrl = '${SUPABASE_URL}';
-  static const String supabaseAnonKey = '${SUPABASE_ANON_KEY}';
-  static const String igdbClientId = '${IGDB_CLIENT_ID}';
-  static const String igdbClientSecret = '${IGDB_CLIENT_SECRET}';
+# Generamos el archivo .env.json usando las variables de entorno inyectadas por Vercel
+cat <<EOF > .env.json
+{
+  "SUPABASE_URL": "$SUPABASE_URL",
+  "SUPABASE_ANON_KEY": "$SUPABASE_ANON_KEY",
+  "IGDB_CLIENT_ID": "$IGDB_CLIENT_ID",
+  "IGDB_CLIENT_SECRET": "$IGDB_CLIENT_SECRET",
+  "FIREBASE_API_KEY": "$FIREBASE_API_KEY",
+  "FIREBASE_AUTH_DOMAIN": "$FIREBASE_AUTH_DOMAIN",
+  "FIREBASE_PROJECT_ID": "$FIREBASE_PROJECT_ID",
+  "FIREBASE_STORAGE_BUCKET": "$FIREBASE_STORAGE_BUCKET",
+  "FIREBASE_MESSAGING_SENDER_ID": "$FIREBASE_MESSAGING_SENDER_ID",
+  "FIREBASE_APP_ID": "$FIREBASE_APP_ID",
+  "FIREBASE_VAPID_KEY": "$FIREBASE_VAPID_KEY"
 }
 EOF
 
 # Descargamos Flutter y compilamos la versión Web
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
-./flutter/bin/flutter build web --release
+./flutter/bin/flutter build web --release --dart-define-from-file=.env.json
