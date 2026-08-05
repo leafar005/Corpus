ee6c6d0892eba53c56978bbd34a8317b946d3202
Rafael Casado <rafael.casado@udc.es>
2026-08-05 11:59:33 +0200

    test(widgets): fix test suite after Game model refactor

--- test/widgets/game_card_test.dart
+++ test/widgets/game_card_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:corpus/widgets/game_card.dart';
+import 'package:corpus/models/models.dart';
 
 void main() {
   group('GameCard Widget Tests', () {
@@ -9,6 +10,7 @@ void main() {
       'Muestra título al hacer hover, nota y carátula cuando tiene datos completos',
       (WidgetTester tester) async {
         final gameMock = {
+          'id': 1,
           'title': 'Elden Ring',
           'cover_url':
               'https://images.igdb.com/igdb/image/upload/t_cover_big/co4j8m.jpg',
@@ -18,7 +20,7 @@ void main() {
           MaterialApp(
             home: Scaffold(
               body: GameCard(
-                game: gameMock,
+                game: Game.fromMap(gameMock),
                 isInLibrary: true,
                 userRating: 9.5,
                 onReturn: () {},
@@ -43,13 +45,13 @@ void main() {
     testWidgets(
       'No muestra nota si el juego no está valorado (userRating = 0)',
       (WidgetTester tester) async {
-        final gameMock = {'title': 'Hollow Knight'};
+        final gameMock = {'id': 2, 'title': 'Hollow Knight'};
 
         await tester.pumpWidget(
           MaterialApp(
             home: Scaffold(
               body: GameCard(
-                game: gameMock,
+                game: Game.fromMap(gameMock),
                 isInLibrary: false,
                 userRating: 0.0,
                 onReturn: () {},
@@ -74,13 +76,13 @@ void main() {
     testWidgets(
       'Muestra un icono genérico (Icons.videogame_asset) cuando la URL de la carátula es nula o vacía',
       (WidgetTester tester) async {
-        final gameMock = {'title': 'Juego Sin Carátula', 'cover_url': ''};
+        final gameMock = {'id': 3, 'title': 'Juego Sin Carátula', 'cover_url': ''};
 
         await tester.pumpWidget(
           MaterialApp(
             home: Scaffold(
               body: GameCard(
-                game: gameMock,
+                game: Game.fromMap(gameMock),
                 isInLibrary: true,
                 userRating: 0.0,
                 onReturn: () {},
